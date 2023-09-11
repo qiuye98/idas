49ba9d25c8af1072550ece5ea1136555f911dcd4
wuboxin <wuboxin98@outlook.com>
2023-09-11 22:32:17 +0800

    nacos服务注册

--- member/src/main/java/cn/idas_ouc/member/MemberApplication.java
+++ member/src/main/java/cn/idas_ouc/member/MemberApplication.java
@@ -3,7 +3,9 @@ package cn.idas_ouc.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("cn.idas_ouc.member.dao")
 public class MemberApplication {
